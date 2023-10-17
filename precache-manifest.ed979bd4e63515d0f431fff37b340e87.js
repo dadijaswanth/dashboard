2b9c01929bb7207ef9da62aecae74086d195e64d
sp0303 <patnayakunisharat@gmail.com>
2023-10-17 13:37:45 +0530

    Updates

---
+++ precache-manifest.ed979bd4e63515d0f431fff37b340e87.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459757ff305a6657986fcbd7657d600d",
+    "url": "/demo/purple-react-free/template/demo_1/preview/index.html"
+  },
+  {
+    "revision": "b838141a65e623204d2e",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/css/4.687196e3.chunk.css"
+  },
+  {
+    "revision": "47c843e3915afffa9f31",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/css/main.b8e6a5b8.chunk.css"
+  },
+  {
+    "revision": "b57be6f921b73c935792",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/0.0b6c4836.chunk.js"
+  },
+  {
+    "revision": "17dae9bbb07a38bf42f1",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/1.6e2e43d6.chunk.js"
+  },
+  {
+    "revision": "cde0ef11c4dfb926ebf2",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/10.e407ea7b.chunk.js"
+  },
+  {
+    "revision": "b5b111d0f819dc4561bf",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/11.66864967.chunk.js"
+  },
+  {
+    "revision": "3a408d1dec265ca50a1b",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/12.be77161d.chunk.js"
+  },
+  {
+    "revision": "38100fe11bd8a2a2f42a",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/13.f8726ac5.chunk.js"
+  },
+  {
+    "revision": "b158c2ad5d872441f9d9",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/14.e3d148a5.chunk.js"
+  },
+  {
+    "revision": "0191c56fccbec9c1ea42",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/15.66ba444e.chunk.js"
+  },
+  {
+    "revision": "957c13b80d11ff504de2",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/16.2b9cb754.chunk.js"
+  },
+  {
+    "revision": "b3924aba7b7eed8ae454",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/17.a9eb3f3c.chunk.js"
+  },
+  {
+    "revision": "4f55d464d2c28e87f737",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/18.9a86c625.chunk.js"
+  },
+  {
+    "revision": "b838141a65e623204d2e",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/4.d685b3c7.chunk.js"
+  },
+  {
+    "revision": "c62d52853ab9f9b8c76d",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/5.2f764c9c.chunk.js"
+  },
+  {
+    "revision": "3371bfa878265e9c67e7",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/6.84fdf913.chunk.js"
+  },
+  {
+    "revision": "81c9671ae7cb6e0d866d",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/7.f5bc5371.chunk.js"
+  },
+  {
+    "revision": "6f1a2a9962570b2e26e1",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/8.ef81854f.chunk.js"
+  },
+  {
+    "revision": "d75c3744cc5d1778df7d",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/9.696121b0.chunk.js"
+  },
+  {
+    "revision": "47c843e3915afffa9f31",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/main.95eea42a.chunk.js"
+  },
+  {
+    "revision": "32f5a8096025ca917c93",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/js/runtime~main.7bb27c06.js"
+  },
+  {
+    "revision": "1674de7c01117c0f50f8831f2f03287a",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/1.1674de7c.jpg"
+  },
+  {
+    "revision": "989ef602831d1fba2d713e3cb9a8175d",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/2.989ef602.jpg"
+  },
+  {
+    "revision": "3aa4e7a357f08247a180e2f547264c30",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/3.3aa4e7a3.jpg"
+  },
+  {
+    "revision": "b41232086571c961a231eaad37e58696",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/abs1.b4123208.jpg"
+  },
+  {
+    "revision": "1541da91140ebcab576c1ef5465fba5e",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/circle.1541da91.svg"
+  },
+  {
+    "revision": "42d41e6133b1ff5a44c537ea957a7ab9",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face1.42d41e61.jpg"
+  },
+  {
+    "revision": "16f59cbaad05b1d49117497f2e6741b0",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face13.16f59cba.jpg"
+  },
+  {
+    "revision": "7e0e382da357aafeebaa825400bb590b",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face2.7e0e382d.jpg"
+  },
+  {
+    "revision": "16c67435545cf63033bb5876a2736a9c",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face3.16c67435.jpg"
+  },
+  {
+    "revision": "d5afaa667746492dad279ef18667e623",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face4.d5afaa66.jpg"
+  },
+  {
+    "revision": "d24172840592b28bba6872ffce4e2843",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face5.d2417284.jpg"
+  },
+  {
+    "revision": "07adc9a9ac3f0a888806a9071a8bf8f6",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face6.07adc9a9.jpg"
+  },
+  {
+    "revision": "7af91f951b43200c0786517742caf90e",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/face7.7af91f95.jpg"
+  },
+  {
+    "revision": "0d7ef3668eba8487d2a5a1dbae48c3c0",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/lockscreen-bg.0d7ef366.jpg"
+  },
+  {
+    "revision": "2eeb0447c0350018da406e77dfec1f10",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/login-bg.2eeb0447.jpg"
+  },
+  {
+    "revision": "8aef5918ff485b9f9a546acf6215ee0c",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/logo-mini.8aef5918.svg"
+  },
+  {
+    "revision": "2211951a272a2eb9da5b0910985f4fcf",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/logo.2211951a.svg"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "2ccde6b9925e2d16454cca89664a03e5",
+    "url": "/demo/purple-react-free/template/demo_1/preview/static/media/register-bg.2ccde6b9.jpg"
+  }
+]);
